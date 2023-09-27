c9924c2e47a12edcbc9391752c4e93dce1bd3ec6
badass1901 <shashwatsagar19@gmail.com>
2023-09-27 06:13:00 -0700

    added local storage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,16 @@ function App() {
   const toggleComplete = (id) =>{
     setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo ))
   }
-  
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem('todos'))
+    if(todos && todos.length > 0){
+      setTodos(todos);
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem('todos', JSON.stringify(todos))
+  },[todos])
  
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
