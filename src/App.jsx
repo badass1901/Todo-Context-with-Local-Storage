8a16704d1efdc9078d0a9683f2593141c219f9b9
badass1901 <shashwatsagar19@gmail.com>
2023-09-27 06:46:30 -0700

    fund bug with toggleComplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { TodoProvider } from "./context";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -17,7 +19,7 @@ function App() {
     setTodos((prev)=> prev.filter((todo)=> todo.id !== id))
   }
   const toggleComplete = (id) =>{
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo ))
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo ))
   }
   useEffect(()=>{
     const todos = JSON.parse(localStorage.getItem('todos'))
@@ -37,9 +39,13 @@ function App() {
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">
           Manage Your Todos
         </h1>
-        <div className="mb-4">{/* Todo form goes here */}</div>
+        <div className="mb-4"> <TodoForm /></div>
         <div className="flex flex-wrap gap-y-3">
-          {/*Loop and Add TodoItem here */}
+          {todos.map((todo)=>(
+            <div key={todo.id} className="w-full">
+              <TodoItem todo={todo} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
